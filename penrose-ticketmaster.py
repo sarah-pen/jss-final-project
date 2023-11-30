189c0de8f5ec1132b10c4cd6aa9dcab9779d1632
Sarah Penrose <penroses003@gmail.com>
2023-11-30 11:41:40 -0500

    added file

---
+++ penrose-ticketmaster.py
@@ -0,0 +1,8 @@
+import requests
+
+# root URL : https://app.ticketmaster.com/{package}/{version}/{resource}.json?apikey=**{API key}
+# resp = requests.get("...")
+# save resp as json
+
+# insert data into database
+
