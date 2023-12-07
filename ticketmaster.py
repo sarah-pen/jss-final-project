7a6e28deda6b0ff1acd14835a5cde231c4196dee
Sarah Penrose <penroses003@gmail.com>
2023-12-06 22:52:53 -0500

    started functions

--- ticketmaster.py
+++ ticketmaster.py
@@ -1,58 +1,84 @@
 import requests
 import json
+import os
 
 # root URL : https://app.ticketmaster.com/{package}/{version}/{resource}.json?apikey=**{API key}
-# apikey = A3phA47g5rC6uF9zpmgWGxlD7SCtsimG
 
-# to find the id of an artist or venue: 
-    # /attractions or /venues in url -> set "keyword" param -> retrieve d["_embedded"]["venues/attractions"][0]["id"]
-
-# create table if not exists
-# read locations from file
-# keep count of number retrieved from APi and stop at 200
 
 def get_data(api_key, page):
+    '''
+    Check whether the 'params' dictionary has been specified. 
+    Makes a request to access data with the 'url' and 'params' given, if any. 
+    If the request is successful, return a dictionary representation 
+    of the decoded JSON. If the search is unsuccessful, print out "Exception!"
+    and return None.
+    '''
 
+    # https://app.ticketmaster.com/discovery/v2/events.json?apikey=A3phA47g5rC6uF9zpmgWGxlD7SCtsimG&keyword=Taylor%20Swift
+    
     resp = requests.get("https://app.ticketmaster.com/discovery/v2/events.json", params={"apikey": api_key, "attractionId": "K8vZ9175Tr0", "page": page})
-    dct = json.loads(resp.content)
-    events = dct["_embedded"]["events"]
-
-    for event in events:
-        city = event["_embedded"]["venues"][0]["city"]["name"]
-
-        if "priceRanges" in event:
-            max_price = str(event["priceRanges"][0]["max"])
-            print(city + ": max " + max_price)
-        else:
-            print(city + ": none")
-
-    return dct["_embedded"]["events"]
+    data = resp.json()
+    return data
 
     
-
 def write_json(filename, dict):
+    '''
+    Encodes dict into JSON format and writes
+    the JSON to filename to save the search results
+    '''
 
     f = open(filename, "w")
     f.write(json.dumps(dict))
     f.close()
 
 
-    # https://app.ticketmaster.com/discovery/v2/events.json?apikey=A3phA47g5rC6uF9zpmgWGxlD7SCtsimG&keyword=Taylor%20Swift
+def load_json(filename):
 
-    # artist = input("What's your favorite music artist (or 'quit')? ")
-    # urls = []
+    '''
+    Loads a JSON cache from filename if it exists and returns dictionary
+    with JSON data or an empty dictionary if the cache does not exist
+    '''
 
-    # while artist != 'quit':
+    try:
+        source_dir = os.path.dirname(__file__)
+        full_path = os.path.join(source_dir, filename)
 
-    #     resp = requests.get("https://app.ticketmaster.com/discovery/v2/attractions.json", params={"apikey": api_key, "keyword": artist})
-    #     dct = json.loads(resp.content)
-    #     url = dct["_embedded"]["attractions"][0]["url"]
-    #     if url not in urls:
-    #         urls.append(url)
-    #     print(url)
-    #     artist = input("What's your favorite music artist (or 'quit')? ")
+        f = open(full_path)
+        contents = f.read()
+        f.close()
 
-    # return urls 
+        data = json.loads(contents)
+
+        return data
+    
+    except:
+        return {}
+    
+
+def cache_all_pages(url, filename):
+    '''
+    1. Checks if the page number is found in the dict return by `load_json`
+    2. If the page number does not exist in the dictionary, it makes a request (using get_swapi_info)
+    3. Add the data to the dictionary (the key is the page number (Ex: page 1) and the value is the results).
+    4. Write out the dictionary to a file using write_json.
+    '''
+    pass
+
+def event_info(filename):
+
+    # dct = json.loads(resp.content)
+    # events = dct["_embedded"]["events"]
+
+    # for event in events:
+    #     city = event["_embedded"]["venues"][0]["city"]["name"]
+
+    #     if "priceRanges" in event:
+    #         max_price = str(event["priceRanges"][0]["max"])
+    #         print(city + ": max " + max_price)
+    #     else:
+    #         print(city + ": none")
+    pass
+
     
 
 
@@ -62,5 +88,6 @@ def main():
     write_json("test.json", data)
 
 
-main()
+if __name__ == "__main__":
+    main()
 
