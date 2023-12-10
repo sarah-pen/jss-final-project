7dc6de8a86b64c4347c5e4ef177d7fea30d52316
okjiki <144192276+okjiki@users.noreply.github.com>
2023-12-09 20:35:01 -0500

    add song genres

--- musixmatch.py
+++ musixmatch.py
@@ -6,13 +6,23 @@ from bs4 import BeautifulSoup
 
 api_key = "4ced0fe04de2090cb8f068ca309e8d96"
 base_url = "https://api.musixmatch.com/ws/1.1/"
+database = 'music.db'
 
-#creates a table if it doesn't exist and inserts or updates the artist's rating.
+#inserts or updates the artist's rating.
 def add_rating(artist_name, rating, database):
     conn = sqlite3.connect(database)
     cursor = conn.cursor()
     cursor.execute('CREATE TABLE IF NOT EXISTS Ratings (artist TEXT PRIMARY KEY, rating INTEGER)')
     cursor.execute('INSERT OR REPLACE INTO Ratings (artist, rating) VALUES (?, ?)', (artist_name, rating))
+    conn.commit()
+    conn.close()
+
+#inserts or updates artist/song genre
+def add_song_genre(artist_name, song_name, genre, database):
+    conn = sqlite3.connect(database)
+    cursor = conn.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS SongGenres (artist TEXT, song TEXT, genre TEXT, PRIMARY KEY (artist, song))")
+    cursor.execute("INSERT OR REPLACE INTO SongGenres (artist, song, genre) VALUES (?, ?, ?)", (artist_name, song_name, genre))
     conn.commit()
     conn.close()
 
@@ -23,9 +33,17 @@ def get_artists_from_db(database):
     cursor.execute("SELECT artist FROM Artists")  
     artists = cursor.fetchall()
     conn.close()
-
     artist_names = [artist[0] for artist in artists]
     return artist_names
+
+#retrieves list of artist names and songs from database
+def get_artists_and_songs(database):
+    conn = sqlite3.connect(database)
+    cursor = conn.cursor()
+    cursor.execute("SELECT artist, song FROM Artists")  
+    data = cursor.fetchall()
+    conn.close()
+    return data
 
 #fetch the artist's rating from the Musixmatch API
 def get_artist_rating_from_musixmatch(artist_name, api_key):
@@ -48,6 +66,35 @@ def get_artist_rating_from_musixmatch(artist_name, api_key):
                 add_rating(artist_name, artist_rating, database)
             return artist_rating
     return "Error or artist not found"
+    
+#fetch song genre from Musixmatch API
+def get_song_genre(api_key, song_name, artist_name=None):
+    base_url_musixmatch = 'https://api.musixmatch.com/ws/1.1/'
+    method = 'track.search'
+    url = base_url_musixmatch + method
+    params = {
+        'q_track': song_name,
+        'apikey': api_key,
+        'format': 'json',
+        'page_size': 150
+    }
+    if artist_name:
+        params['q_artist'] = artist_name
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        track_list = data.get("message", {}).get("body", {}).get("track_list", [])
+        for track_info in track_list:
+            track = track_info.get("track", {})
+            if track.get("track_name").lower() == song_name.lower() and \
+               (artist_name is None or track.get("artist_name").lower() == artist_name.lower()):
+                genre_list = track.get("primary_genres", {}).get("music_genre_list", [])
+                if genre_list:
+                    genres = [genre.get("music_genre", {}).get("music_genre_name", "") for genre in genre_list]
+                    return ', '.join(genres)
+        return "Genre not found or exact match not found"
+    else:
+        return f"Error: {response.status_code}"
 
 def main():
     #test_artists = ["Queen", "The Beatles", "Adele", "One Direction"]
@@ -62,6 +109,15 @@ def main():
             print(f"Updated rating for Artist: {artist}, Rating: {rating}")
         else:
             print(f"Rating not found for Artist: {artist}")
+
+    artists_and_songs = get_artists_and_songs(database)
+        for artist, song in artists_and_songs:
+            genre = get_song_genre(api_key, song, artist)
+            if genre:
+                add_song_genre(artist, song, genre, database)
+                print(f"Updated genre for Artist: {artist}, Song: {song}, Genre: {genre}")
+            else:
+                print(f"Genre not found for Artist: {artist}, Song: {song}")
 
 if __name__ == "__main__":
     main()
