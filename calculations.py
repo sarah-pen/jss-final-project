e1a3ce7d6c58672401c000a7f9fff85d4272d465
Sarah Penrose <penroses003@gmail.com>
2023-12-11 21:57:54 -0500

    started calculations

--- calculations.py
+++ calculations.py
@@ -1,3 +1,17 @@
+import lastfm
+import musixmatch
+import ticketmaster
+
 # Select items from the tables and calculate something from the data (average, counts, etc)
 # At least one database join used when selecting the data
 # Write a well-formatted, self explanatory file from the calculations (JSON, csv or text file)
+
+def calc_ticketmaster(cur, conn):
+
+    d = {}
+    for i in range(1,7):
+        cur.execute('SELECT min_price FROM Events_final WHERE artist_id=?', (i,))
+        prices = cur.fetchall()[0]
+        
+
+    
