335008e45d7bf50618db868bb58870f9d5d66e70
Sarah Penrose <penroses003@gmail.com>
2023-12-11 20:28:17 -0500

    -

---
+++ calculations.py
@@ -0,0 +1,3 @@
+# Select items from the tables and calculate something from the data (average, counts, etc)
+# At least one database join used when selecting the data
+# Write a well-formatted, self explanatory file from the calculations (JSON, csv or text file)
